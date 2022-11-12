6b9e7e79e0ae178d25bce119507d16bf3c37096c
isXander <xandersmith2008@gmail.com>
2022-11-12 13:28:44 +0000

    little changes

--- src/main/java/dev/isxander/yacl/impl/utils/YACLConstants.java
+++ src/main/java/dev/isxander/yacl/impl/utils/YACLConstants.java
@@ -3,6 +3,6 @@ package dev.isxander.yacl.impl.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class YaclConstants {
+public class YACLConstants {
     public static final Logger LOGGER = LoggerFactory.getLogger("YetAnotherConfigLib");
 }
