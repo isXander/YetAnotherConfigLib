0cca24131ded2eb0c348732369f337286e1cba9f
isXander <xandersmith2008@gmail.com>
2023-05-22 19:35:03 +0100

    fix build

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -26,8 +26,6 @@ dependencies {
 
     implementation(libs.twelvemonkeys.imageio.core)
     implementation(libs.twelvemonkeys.imageio.webp)
-    include(libs.twelvemonkeys.imageio.core)
-    include(libs.twelvemonkeys.imageio.webp)
 }
 
 java {
