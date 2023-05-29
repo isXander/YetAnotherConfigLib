b4344763d905030dbcb840eefc361bbdb3729d99
isXander <xander@isxander.dev>
2023-05-29 07:40:55 +0100

    Add missing `DoubleSliderControllerBuilder.create`

--- common/src/main/java/dev/isxander/yacl/api/controller/DoubleSliderControllerBuilder.java
+++ common/src/main/java/dev/isxander/yacl/api/controller/DoubleSliderControllerBuilder.java
@@ -1,4 +1,10 @@
 package dev.isxander.yacl.api.controller;
 
+import dev.isxander.yacl.api.Option;
+import dev.isxander.yacl.impl.controller.DoubleSliderControllerBuilderImpl;
+
 public interface DoubleSliderControllerBuilder extends SliderControllerBuilder<Double, DoubleSliderControllerBuilder> {
+    static DoubleSliderControllerBuilder create(Option<Double> option) {
+        return new DoubleSliderControllerBuilderImpl(option);
+    }
 }
