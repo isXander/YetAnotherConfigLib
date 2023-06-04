f33bd6a62fb11648fbdc8a39c0b06768dee324ad
isXander <xandersmith2008@gmail.com>
2023-06-04 16:47:01 +0100

    Fix forge modid

--- forge/src/main/java/dev/isxander/yacl/forge/YACLForgeEntrypoint.java
+++ forge/src/main/java/dev/isxander/yacl/forge/YACLForgeEntrypoint.java
@@ -2,7 +2,7 @@ package dev.isxander.yacl3.forge;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("yet_another_config_lib")
+@Mod("yet_another_config_lib_v3")
 public class YACLForgeEntrypoint {
     public YACLForgeEntrypoint() {
 
