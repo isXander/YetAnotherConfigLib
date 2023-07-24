a75211aeb8119cc6c619e7e585a61d42d831afaa
isXander <xandersmith2008@gmail.com>
2023-07-24 16:11:03 +0100

    Bump version + changelog

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ architectury {
     minecraft = libs.versions.minecraft.get()
 }
 
-version = "3.0.3+1.20"
+version = "3.1.0+1.20"
 
 val isBeta = "beta" in version.toString()
 val changelogText = rootProject.file("changelogs/${project.version}.md").takeIf { it.exists() }?.readText() ?: "No changelog provided."
