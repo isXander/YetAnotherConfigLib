4598585d0a7fbbddc476873822c5a4d98f4e0fb6
isXander <xandersmith2008@gmail.com>
2023-10-29 14:53:39 +0000

    Experimental image filtering

--- common/src/main/java/dev/isxander/yacl3/gui/image/impl/AnimatedDynamicTextureImage.java
+++ common/src/main/java/dev/isxander/yacl3/gui/image/impl/AnimatedDynamicTextureImage.java
@@ -1,8 +1,11 @@
 package dev.isxander.yacl3.gui.image.impl;
 
 import com.mojang.blaze3d.Blaze3D;
+import com.mojang.blaze3d.platform.GlConst;
+import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.platform.NativeImage;
 import com.twelvemonkeys.imageio.plugins.webp.WebPImageReaderSpi;
+import dev.isxander.yacl3.debug.DebugProperties;
 import dev.isxander.yacl3.gui.image.ImageRendererFactory;
 import dev.isxander.yacl3.impl.utils.YACLConstants;
 import net.minecraft.CrashReport;
@@ -62,6 +65,12 @@ public class AnimatedDynamicTextureImage extends DynamicTextureImage {
         graphics.pose().pushPose();
         graphics.pose().translate(x, y, 0);
         graphics.pose().scale(ratio, ratio, 1);
+
+        if (DebugProperties.IMAGE_FILTERING) {
+            GlStateManager._texParameter(GlConst.GL_TEXTURE_2D, GlConst.GL_TEXTURE_MAG_FILTER, GlConst.GL_LINEAR);
+            GlStateManager._texParameter(GlConst.GL_TEXTURE_2D, GlConst.GL_TEXTURE_MIN_FILTER, GlConst.GL_LINEAR);
+        }
+
         graphics.blit(
                 uniqueLocation,
                 0, 0,
