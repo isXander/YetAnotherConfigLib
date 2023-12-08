3bf9187f3dda47dd1cf9ce5a65f786ef30be3164
Superkat32 <superkat1806@gmail.com>
2023-12-07 20:08:37 -0500

    Color picker visual redesign
    REFACTOR: Completely redesigned the color picker's looks, having an inventory container like background now.
    REFACTOR: I think I increased the hue slider thumb's height by 2 pixels to adjust for the new redesign to stay satisfying
    FIX: Fixed the colorPickerDim.y and colorPickerDim.yLimit being flipped(that was easier and harder than I thought it would be both at the same time)
    FIX: Cleaned up a bunch of messy rendering code. Turns out, it worked, but wasn't as easy to change as I thought. Should be somewhat better now, I might come back and clean it up some more though

--- common/src/main/java/dev/isxander/yacl3/gui/controllers/ColorPickerElement.java
+++ common/src/main/java/dev/isxander/yacl3/gui/controllers/ColorPickerElement.java
@@ -20,17 +20,15 @@ public class ColorPickerElement extends ControllerWidget<ColorController> implem
     protected MutableDimension<Integer> hueGradientDim;
     private boolean mouseDown;
 
-    //The width of the black outline for the entire color picker
-    //Space is made in between various parts of the color picker using this variable
-    //example: If the outline is set to 1, then a 1 pixel wide black outline is rendered around
-    //the entire color picker, the main color preview, HSL gradient, and hue gradient
+    //The width of the outline between each color picker element(color preview, saturation/light gradient, hue gradient)
+    //Note: Additional space may need to be manually made upon increasing the outline
     private int outline = 1;
 
-    //The x padding in pixels between both color picker parts and the outline of a color picker part
+    //The x padding between the color preview and saturation/light gradient. Does NOT account for the outline on its own
     private int paddingX = 1;
 
-    //The y padding in pixels between both color picker parts and the outline of a color picker part
-    private int paddingY = 1;
+    //The y padding between the hue gradient and color preview & saturation/light gradient. Does NOT account for the outline on its own
+    private int paddingY = 3;
 
     //The main color preview's portion of the color picker as a whole
     //example: if previewPortion is set to 7, then the color preview will take up
@@ -64,42 +62,42 @@ public class ColorPickerElement extends ControllerWidget<ColorController> implem
 
         updateHSL();
 
-        //FIXME - If the color picker is towards the top of the category, it will appear above the color controller instead of below
+        //FIXME/FEAT - If the color picker is towards the top of the category, it will appear above the color controller instead of below
 
         //Main color preview
-        graphics.fill(colorPickerDim.x(), colorPickerDim.y() - sliderHeight - paddingY * 2 - outline, colorPickerDim.x() + (colorPickerDim.xLimit() / previewPortion) - paddingX, colorPickerDim.yLimit(), 3, colorController.option().pendingValue().getRGB());
+        graphics.fill(colorPickerDim.x(), colorPickerDim.y(), colorPickerDim.x() + (colorPickerDim.xLimit() / previewPortion) - paddingX, colorPickerDim.yLimit() - sliderHeight - paddingY, 3, colorController.option().pendingValue().getRGB());
         //outline
-        graphics.fill(colorPickerDim.x() - outline, colorPickerDim.y() - sliderHeight - paddingY * 2, colorPickerDim.x() + (colorPickerDim.xLimit() / previewPortion) - paddingX + outline, colorPickerDim.yLimit() - outline, 2, Color.black.getRGB());
+        graphics.fill(colorPickerDim.x() - outline, colorPickerDim.y() - outline, colorPickerDim.x() + (colorPickerDim.xLimit() / previewPortion) - paddingX + outline, colorPickerDim.yLimit() - sliderHeight - paddingY + outline, 2, Color.black.getRGB());
 
         //HSL gradient
 
         //White to pending color's RGB from hue, left to right
-        fillSidewaysGradient(graphics, colorPickerDim.xLimit(), colorPickerDim.y() - sliderHeight - paddingY * 2 - outline, colorPickerDim.x() + (colorPickerDim.xLimit() / previewPortion) + paddingX + outline, colorPickerDim.yLimit(), 3, 0xFFFFFFFF, (int) getRgbFromHue());
+        fillSidewaysGradient(graphics, colorPickerDim.x() + (colorPickerDim.xLimit() / previewPortion) + paddingX + 1, colorPickerDim.y(), colorPickerDim.xLimit(), colorPickerDim.yLimit() - sliderHeight - paddingY, 3, (int) getRgbFromHue(), 0xFFFFFFFF);
 
         //Transparent to black, top to bottom
-        graphics.fillGradient(colorPickerDim.xLimit(), colorPickerDim.y() - sliderHeight - paddingY * 2 - outline, colorPickerDim.x() + (colorPickerDim.xLimit() / previewPortion) + paddingX + outline, colorPickerDim.yLimit(), 4,0xFF000000, 0x00000000);
+        graphics.fillGradient(colorPickerDim.x() + (colorPickerDim.xLimit() / previewPortion) + paddingX + 1, colorPickerDim.y(), colorPickerDim.xLimit(), colorPickerDim.yLimit() - sliderHeight - paddingY, 4,0x00000000, 0xFF000000);
 
         //outline
-        graphics.fill(colorPickerDim.xLimit() + outline, colorPickerDim.y() - sliderHeight - paddingY * 2, colorPickerDim.x() + (colorPickerDim.xLimit() / previewPortion) + paddingX, colorPickerDim.yLimit() - outline, 2,Color.black.getRGB());
+        graphics.fill(colorPickerDim.x() + (colorPickerDim.xLimit() / previewPortion) + paddingX + 1 - outline, colorPickerDim.y() - outline, colorPickerDim.xLimit() + outline, colorPickerDim.yLimit() - sliderHeight - paddingY + outline, 2, Color.black.getRGB());
+
+
 
         //Hue gradient
 
         //Hue rainbow gradient
-        drawRainbowGradient(graphics, colorPickerDim.x(), colorPickerDim.y(), colorPickerDim.xLimit(), colorPickerDim.y() - sliderHeight, 3);
+        drawRainbowGradient(graphics, colorPickerDim.x(), colorPickerDim.yLimit(), colorPickerDim.xLimit(), colorPickerDim.yLimit() - sliderHeight, 3);
 
         //Hue slider thumb
-        graphics.fill(getHueThumbX() - getThumbWidth() / 2, colorPickerDim.y(), getHueThumbX() + getThumbWidth() / 2, colorPickerDim.y() - sliderHeight, 5, -1);
+        graphics.fill(getHueThumbX() - getThumbWidth() / 2, colorPickerDim.yLimit() + outline, getHueThumbX() + getThumbWidth() / 2, colorPickerDim.yLimit() - sliderHeight - outline, 5, -1);
         //Hue slider thumb shadow
-        graphics.fill(getHueThumbX() - getThumbWidth() / 2 - 1, colorPickerDim.y() + 1, getHueThumbX() + getThumbWidth() / 2 + 1, colorPickerDim.y() - sliderHeight - 1, 4, 0xFF404040);
+        graphics.fill(getHueThumbX() - getThumbWidth() / 2 - 1, colorPickerDim.yLimit() + outline + 1, getHueThumbX() + getThumbWidth() / 2 + 1, colorPickerDim.yLimit() - sliderHeight - outline - 1, 4, 0xFF404040);
 
         //outline
-        graphics.fill(colorPickerDim.x() - outline, colorPickerDim.y() + outline, colorPickerDim.xLimit() + outline, colorPickerDim.y() - sliderHeight - outline, 2, Color.black.getRGB());
+        graphics.fill(colorPickerDim.x() - outline, colorPickerDim.yLimit() + outline, colorPickerDim.xLimit() + outline, colorPickerDim.yLimit() - sliderHeight - outline, 2, Color.black.getRGB());
 
 
         //Background
-        //Space was added between the color preview, HSL gradient, and hue gradient earlier
-//        graphics.fill(colorPickerDim.x() - outline, colorPickerDim.y() + outline, colorPickerDim.xLimit() + outline, colorPickerDim.yLimit() - outline, 1, 0xFF000000);
-        drawBackground(graphics, colorPickerDim.x() - outline, colorPickerDim.y() + outline, colorPickerDim.xLimit() + outline, colorPickerDim.yLimit() - outline, 1);
+        drawBackground(graphics, colorPickerDim.x() - outline, colorPickerDim.y() - outline, colorPickerDim.xLimit() + outline, colorPickerDim.yLimit() + outline, 1);
     }
 
     public void drawBackground(GuiGraphics graphics, int x1, int y1, int x2, int y2, int z) {
@@ -111,30 +109,29 @@ public class ColorPickerElement extends ControllerWidget<ColorController> implem
         int grey = new Color(0xC6C6C6).getRGB();
 
         //The center color(grey)
-        graphics.fill(x1 - 1, y1 + 1, x2 + 1, y2 - 1, z, grey);
-        drawRoundOutline(graphics, x1 - 1, y1 + 1, x2 + 1, y2 - 1, z, grey, 1);
+        graphics.fill(x1 - 1, y1 - 1, x2 + 1, y2 + 1, z, grey);
+        drawRoundOutline(graphics, x1 - 1, y1 - 1, x2 + 1, y2 + 1, z, grey, 1);
 
         //The white outline around the grey
-        drawRoundOutline(graphics, x1 - 2, y1 + 2, x2 + 2, y2 - 2, z, Color.white.getRGB(), 1);
+        drawRoundOutline(graphics, x1 - 2, y1 - 2, x2 + 2, y2 + 2, z, Color.white.getRGB(), 1);
 
         //The black outline around the whole thing
-//        drawRoundOutline(graphics, x1 - 4, y1 + 4, x2 + 4, y2 - 4, z, Color.black.getRGB(), 2);
-        drawRoundOutline(graphics, x1 - 3, y1 + 3, x2 + 3, y2 - 3, z, Color.black.getRGB(), 1);
+        drawRoundOutline(graphics, x1 - 3, y1 - 3, x2 + 3, y2 + 3, z, Color.black.getRGB(), 1);
     }
 
     protected void drawRoundOutline(GuiGraphics graphics, int x1, int y1, int x2, int y2, int z, int color, int pixels) {
-        graphics.fill(x1, y2, x2, y2 - pixels, z, color); //top bar
-        graphics.fill(x1, y1 + pixels, x2, y1, z, color); //bottom bar
-        graphics.fill(x1 - pixels, y1, x1, y2, z, color); //left bar
+        graphics.fill(x1, y1, x2, y1 - pixels, z, color); //top bar
+        graphics.fill(x1, y2, x2, y2 + pixels, z, color); //bottom bar
+        graphics.fill(x1, y1, x1 - pixels, y2, z, color); //left bar
         graphics.fill(x2, y1, x2 + pixels, y2, z, color); //right bar
-        graphics.fill(x2 - pixels, y2 + pixels, x2, y2, z, color); //top right square
-        graphics.fill(x1 + pixels, y2 + pixels, x1, y2, z, color); //top left square
-        graphics.fill(x2 - pixels, y1 - pixels, x2, y1, z, color); //bottom right square
-        graphics.fill(x1 + pixels, y1 - pixels, x1, y1, z, color); //bottom left square
+        graphics.fill(x1 + pixels, y1 - pixels, x1, y1 + pixels, z, color); //top left square
+        graphics.fill(x2 - pixels, y1 - pixels, x2, y1 + pixels, z, color); //top right square
+        graphics.fill(x1 + pixels, y2, x1, y2 - pixels, z, color); //bottom left square
+        graphics.fill(x2 - pixels, y2, x2, y2 - pixels, z, color); //bottom right square
     }
 
     public boolean clickedHueSlider(double mouseX, double mouseY) {
-        if(mouseY <= colorPickerDim.y() && mouseY >= colorPickerDim.y() - sliderHeight) {
+        if(mouseY <= colorPickerDim.yLimit() && mouseY >= colorPickerDim.yLimit() - sliderHeight) {
             //mini workaround for holding the mouse down past the x/xLimit
             return mouseDown || mouseX >= colorPickerDim.x() && mouseX <= colorPickerDim.xLimit();
         }
@@ -167,8 +164,9 @@ public class ColorPickerElement extends ControllerWidget<ColorController> implem
     @Override
     public boolean isMouseOver(double mouseX, double mouseY) {
         //Checks if the mouse is either over the color picker or the color controller
-        if (mouseX >= colorPickerDim.x() && mouseX <= colorPickerDim.xLimit()
-                && mouseY >= colorPickerDim.yLimit() && mouseY <= colorPickerDim.y()) { //y and yLimit flipped apparently?
+        //The addition/subtraction of the outline and extra 3 pixels is to account for both the outline and the background
+        if (mouseX >= colorPickerDim.x() - outline - 3 && mouseX <= colorPickerDim.xLimit() + outline + 3
+                && mouseY >= colorPickerDim.y() - outline - 3 && mouseY <= colorPickerDim.yLimit() + outline + 3) {
             return true;
         }
         return false;
@@ -205,15 +203,12 @@ public class ColorPickerElement extends ControllerWidget<ColorController> implem
     public void setDimension(Dimension<Integer> dim) {
         super.setDimension(dim);
 
-        int colorPickerHeight = (dim.height() * -2) - 7;
+        int colorPickerHeight = (dim.height() * 2) + 7;
         int colorPickerX = dim.centerX() - getXPadding() * 2;
 
         //A single dimension for the entire color picker as a whole
-        //Would allow for the x/y(Limit) to work for the outline by adding + 1 to everything
-        //Division would be used for the bigger color preview, light and saturation picker, and hue slider
-        //to determine their dimensions
-        //FIXME - y/yLimit are flipped? - (dim.height() * 2) - 7 ???
-        colorPickerDim = Dimension.ofInt(colorPickerX, dim.y(), (dim.width() + dim.x() - colorPickerX), colorPickerHeight);
+        //Division is used for the main color preview, saturation/light picker, and hue slider to determine their dimensions
+        colorPickerDim = Dimension.ofInt(colorPickerX, dim.y() - colorPickerHeight - sliderHeight, dim.xLimit() - colorPickerX, colorPickerHeight);
     }
 
     @Override
@@ -247,6 +242,11 @@ public class ColorPickerElement extends ControllerWidget<ColorController> implem
         int max = colorPickerDim.xLimit();
         int value = (int) (colorPickerDim.x() + colorPickerDim.width() * this.hue);
 
+        //Checks if the hue is #FF0001
+        //This is done to keep symmetry when the slider is fully on the left or right
+        if(this.hue > 0.999f) {
+            value++;
+        }
         return Mth.clamp(value, min, max);
     }
 
