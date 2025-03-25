ab0b0a720bcca110d4af9b72a164bb442d0f66ba
isxander <xander@isxander.dev>
2025-03-25 16:49:08 +0000

    1.21.5 release (with neoforge)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ stonecutter {
             }
         }
 
-        mc("1.21.5", loaders = listOf("fabric"))
+        mc("1.21.5", loaders = listOf("fabric", "neoforge"))
         mc("1.21.4", loaders = listOf("fabric", "neoforge"))
         mc("1.21.3", loaders = listOf("fabric", "neoforge"))
         mc("1.21.1", loaders = listOf("fabric", "neoforge"))
