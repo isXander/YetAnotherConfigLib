f94f8677d431cff0a543a084f8120c16c99631d2
isxander <xander@isxander.dev>
2026-01-06 16:46:37 +0000

    update modstitch + stonecutter

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.kikugie.stonecutter") version "0.7.11"
+    id("dev.kikugie.stonecutter") version "0.8.2"
 }
 
 stonecutter {
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,12 +1,10 @@
 plugins {
-    base
     kotlin("jvm") version "2.2.21" apply false
 
     id("dev.kikugie.stonecutter")
 
-    val modstitchVersion = "0.7.1-unstable"
+    val modstitchVersion = "0.8.1"
     id("dev.isxander.modstitch.base") version modstitchVersion apply false
-    id("fabric-loom") version "1.13-SNAPSHOT" apply false
 
     id("me.modmuss50.mod-publish-plugin") version "0.8.4" apply false
     id("org.ajoberstar.grgit") version "5.0.+" apply false
@@ -15,10 +13,21 @@ stonecutter active file("versions/current")
 
 allprojects {
     repositories {
-        maven("https://maven.terraformersmc.com")
-        maven("https://maven.isxander.dev/releases")
-        maven("https://maven.isxander.dev/snapshots")
-        maven("https://maven.quiltmc.org/repository/release")
+        exclusiveContent {
+            forRepository { maven("https://maven.terraformersmc.com") }
+            filter { includeGroup("com.terraformersmc") }
+        }
+        exclusiveContent {
+            forRepositories(
+                maven("https://maven.isxander.dev/releases"),
+                maven("https://maven.isxander.dev/snapshots"),
+            )
+            filter { includeGroup("dev.isxander") }
+        }
+        exclusiveContent {
+            forRepository { maven("https://maven.quiltmc.org/repository/release") }
+            filter { includeGroupAndSubgroups("org.quiltmc") }
+        }
         exclusiveContent {
             forRepository { maven("https://thedarkcolour.github.io/KotlinForForge/") }
             filter { includeGroup("thedarkcolour") }
@@ -29,17 +38,19 @@ allprojects {
 
 stonecutter {
     parameters {
-        fun String.propDefined() = project(node.metadata.project).findProperty(this)?.toString()?.isNotBlank() ?: false
-        constants += listOf(
-            "controlify" to "deps.controlify".propDefined(),
-            "mod-menu" to "deps.modMenu".propDefined(),
-        )
+        constants {
+            fun String.propDefined() = project(node.metadata.project).findProperty(this)?.toString()?.isNotBlank() ?: false
+
+            put("controlify", "deps.controlify".propDefined())
+            put("mod-menu", "deps.modMenu".propDefined())
+        }
     }
 }
 
 version = property("modVersion") as String
 
-tasks.clean {
+tasks.register("clean") {
+    group = "build"
     delete(layout.buildDirectory.dir("finalJars"))
 }
 
