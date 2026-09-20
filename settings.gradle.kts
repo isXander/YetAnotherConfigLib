b368704792b81b20040581cfab9643a124bbfedd
Simon Felix Conrad <simon.felix.conrad@proton.me>
2026-09-20 14:27:35 +0200

    Add NeoForge 26.3 support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,7 +25,7 @@ stonecutter {
 
         mc("26.1", loaders = listOf("fabric", "neoforge"))
         mc("26.2", loaders = listOf("fabric", "neoforge"))
-        mc("26.3", loaders = listOf("fabric"))
+        mc("26.3", loaders = listOf("fabric", "neoforge"))
     }
 }
 rootProject.name = "YetAnotherConfigLib"
